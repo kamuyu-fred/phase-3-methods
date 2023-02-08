3c47abf50064f3f7a94fe287f771d93d4ae4b85c
fred <daktarispacetime@gmail.com>
2023-02-08 05:27:44 +0300

    done

--- methods.rb
+++ methods.rb
@@ -3,5 +3,22 @@ def greet_programmer()
     puts 'Hello, programmer!'
 end
 
-def greet (name )
-    puts "Hello,#{Name}!"
+def greet (name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add a, b
+    return a + b
+end
+
+def halve num
+    if num.class != Integer
+    return nil
+    end
+
+    num/2
+end
